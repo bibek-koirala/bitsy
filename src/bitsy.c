db5094ecb7b1096af2f8c34208d3a49b44de8bd0
bibekji990 <bibekji990@gmail.com>
2018-11-18 14:46:17 +0200

    Disabled terminal's default canonical mode for bitsy by flipping flags' bits

--- src/bitsy.c
+++ src/bitsy.c
@@ -1,6 +1,30 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <unistd.h>
+#include <termios.h>
+
+struct termios origTermInfo;
+
+void enableCanonicalMode () {
+   tcsetattr(STDIN_FILENO, TCSAFLUSH, &origTermInfo);
+}
+
+// Disables terminal's default canonical mode just for bitsy. ECHO, ICANON, ISIG , IXON, IEXTEN, ICRNL, OPOST are individual bits in c_lflag.
+void disableCanonicalMode () {
+  tcgetattr(STDIN_FILENO, &origTermInfo);
+  atexit(enableCanonicalMode);
+
+  struct termios termInfo = origTermInfo;
+  tcgetattr(STDIN_FILENO, &termInfo);
+  termInfo.c_lflag &= ~(IXON | ICRNL);
+  termInfo.c_oflag &= ~(OPOST);
+  termInfo.c_lflag &= ~(ECHO | ICANON | ISIG | IEXTEN);    // flipping bitflags and then using bitmask c_lflag
+  tcsetattr(STDIN_FILENO, TCSAFLUSH, &termInfo);
+}
 
 int main () {
-  printf("Hi Console");
+  char c ;
+  while ( read(STDIN_FILENO, &c, 1) == 1);
   return 0;
 }
