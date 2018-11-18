2648dc8d5ebf94cbfa4749b48444a409f4faf047
bibekji990 <bibekji990@gmail.com>
2018-11-18 17:47:58 +0200

    Added minimal  error handling code

--- src/bitsy.c
+++ src/bitsy.c
@@ -3,27 +3,39 @@
 #include <ctype.h>
 #include <unistd.h>
 #include <termios.h>
+#include <errno.h>
 
 struct termios origTermInfo;
 
-void enableCanonicalMode () {
-   tcsetattr(STDIN_FILENO, TCSAFLUSH, &origTermInfo);
+// Error handler
+void die (const char * errorMsg){
+  perror(errorMsg);
+  exit(1);
 }
 
-// Disables terminal's default canonical mode just for bitsy. ECHO, ICANON, ISIG , IXON, IEXTEN, ICRNL, OPOST are individual bits in c_lflag.
+void enableCanonicalMode () {
+  if(tcsetattr(STDIN_FILENO, TCSAFLUSH, &origTermInfo) == -1 )
+      die(tcsetattr);
+}
+
+// ECHO, ICANON, ISIG , IXON, IEXTEN, ICRNL are individual bits in c_lflag.
 void disableCanonicalMode () {
-  tcgetattr(STDIN_FILENO, &origTermInfo);
+  if(tcgetattr(STDIN_FILENO, &origTermInfo) == -1)
+     die(tcgetattr);
   atexit(enableCanonicalMode);
 
   struct termios termInfo = origTermInfo;
   tcgetattr(STDIN_FILENO, &termInfo);
   termInfo.c_lflag &= ~(IXON | ICRNL);
   termInfo.c_oflag &= ~(OPOST);
-  termInfo.c_lflag &= ~(ECHO | ICANON | ISIG | IEXTEN);    // flipping bitflags and then using bitmask c_lflag
-  tcsetattr(STDIN_FILENO, TCSAFLUSH, &termInfo);
+  termInfo.c_lflag &= ~(ECHO | ICANON | ISIG | IEXTEN); 
+  if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &termInfo) == -1)
+     die(tcsetattr);
 
-  termInfo.c_cc[VMIN] = 0;    // Minimum 0 byte : reads as soon as input is available
-  termInfo.c_cc[VTIME] = 1;   // Wait 100 ms before returning read().
+  //  reads as soon as input is available
+  termInfo.c_cc[VMIN] = 0;    
+  // Wait 100 ms before returning read().
+  termInfo.c_cc[VTIME] = 1;   
 }
 
 int main () {
@@ -31,8 +43,10 @@ int main () {
 
   while (1) {
     char c ;
-    read(STDIN_FILENO, &c, 1);
-    if (!iscntrl(c)) {                     // Displaying only non-control characters
+    if (read(STDIN_FILENO, &c, 1) == -1) 
+        die("read");
+    // Displaying only non-control characters
+    if ( !iscntrl(c) ) {                     
       printf("%c\r\n", c);
     }
 
