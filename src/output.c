27f35fe94d7df75c096c91408e360851397f71f6
bibek-koirala <bibekji990@gmail.com>
2018-11-24 05:58:35 +0200

    Added welcome message to the centre of 1/3rd of screen for now

--- src/output.c
+++ src/output.c
@@ -4,6 +4,7 @@
 #include "terminal.h"
 #include "buffer.h"
 
+#define BITSY_VERSION "0.1"
 #define ABUF_INIT {NULL, 0}
 
 struct editorConfig E;
@@ -11,7 +12,27 @@ struct editorConfig E;
 void editorDrawRows(struct appendBuffer *abuf) {
   int tildes;
   for (tildes = 0; tildes < E.screenRows; tildes++) {
-     abufAppend(abuf, "~", 1);
+    // Display welcome message at one-third of screen from top
+     if (tildes == E.screenRows / 3) {
+        char welcome[80];
+        int welcomeLen = snprintf(welcome, sizeof(welcome),
+          "bitsy editor -- version %s", BITSY_VERSION);
+        if (welcomeLen > E.screenCols) welcomeLen = E.screenCols;
+        // Center the welcome message on screen
+        int padding = (E.screenCols - welcomeLen) / 2;
+        if (padding) {
+           abufAppend(abuf, "~", 1);
+           padding--;
+        }
+        while (padding--) abufAppend(abuf, " ", 1);
+        abufAppend(abuf, welcome, welcomeLen);
+     }
+
+     else {
+        abufAppend(abuf, "~", 1);
+     }
+     // Clear a line/row 
+     abufAppend(abuf, "\x1b[K", 3);
      if (tildes < E.screenRows - 1) {
         abufAppend( abuf, "\r\n", 2);
     }
@@ -22,8 +43,6 @@ void editorRefreshScreen() {
   struct appendBuffer abuf = ABUF_INIT;
   // hiding the cursor to avoid flicker when screen refreshes
   abufAppend(&abuf, "\x1b[?25l", 6);
-  // clearing the entire screen 
-  abufAppend(&abuf, "\x1b[2J", 4);
   // Repositioning the cursor to top-left corner
   abufAppend(&abuf, "\x1b[H", 3);
 
