e3217deebe5abb3d05efd6144b402b69a06a7f0f
bibek-koirala <bibekji990@gmail.com>
2018-11-25 04:59:55 +0200

    Fix : second commit for PageUp-PageDown since terminal.c was not commited previously

--- src/terminal.c
+++ src/terminal.c
@@ -61,12 +61,23 @@ int editorReadKey () {
       if (read(STDIN_FILENO, &seq[0], 1) != 1) return '\x1b';
       if (read(STDIN_FILENO, &seq[1], 1) != 1) return '\x1b';
       if (seq[0] == '[') {
-         switch (seq[1]) {
-            case 'A': return ARROW_UP;
-            case 'B': return ARROW_DOWN;
-            case 'C': return ARROW_RIGHT;
-            case 'D': return ARROW_LEFT;
-         }
+          if (seq[1] >= '0' && seq[1] <= '9') {
+              if (read(STDIN_FILENO, &seq[2], 1) != 1) return '\x1b';
+              if (seq[2] == '~') {
+                switch (seq[1]) {
+                   case '5': return PAGE_UP;
+                   case '6': return PAGE_DOWN;
+                }
+              } 
+          }
+          else {
+             switch (seq[1]) {
+                  case 'A': return ARROW_UP;
+                  case 'B': return ARROW_DOWN;
+                  case 'C': return ARROW_RIGHT;
+                  case 'D': return ARROW_LEFT;
+              }
+          }
       }
       return '\x1b';
   } 
